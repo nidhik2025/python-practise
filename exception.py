0979f01bf1731e6f152818d84642771b8f5028c1
nidhik2025 <nidhi.nivzen@gmail.com>
2025-03-06 11:22:28 +0530

    Add files via upload

--- exception.py
+++ exception.py
@@ -12,3 +12,69 @@ except ValueError:
     print("you didn't enter valid age")
 
 print("execution continuous")
+
+try:
+    age = int(input("age:"))
+    xfactor = 10/age
+
+except ValueError:
+    print("you didn't enter valid age")
+except ZeroDivisionError:
+    print("age cannot be zero")
+else:
+    print(" no exception were thrown ")
+
+# cleaning up
+try:
+    file = open("exception.py")
+    age = int(input("age:"))
+    xfactor = 10/age
+except ValueError:
+    print("you didn't enter valid age")
+except ZeroDivisionError:
+    print("age cannot be zero")
+else:
+    print(" no exception were thrown ")
+finally:
+    file.close()
+
+# with statement
+try:
+    with open("exception.py") as file, open("another.txt") as target:
+        print("file opened")
+        # file.__exit__
+    age = int(input("age:"))
+    xfactor = 10/age
+except ValueError:
+    print("you didn't enter valid age")
+except ZeroDivisionError:
+    print("age cannot be zero")
+else:
+    print(" no exception were thrown ")
+finally:
+    file.close()
+
+# raise exception
+def calculate(age):
+    if age<=0:
+        raise ValueError("age cannot be 0 or less")
+    return age/10
+try:
+    calculate(-2)
+except ValueError as error:
+    print(error)
+
+from timeit import timeit
+
+code1 = "" 
+def calculate(age):
+    if age<=0:
+        raise ValueError("age cannot be 0 or less")
+    return age/10
+try:
+    calculate(-2)
+except ValueError as error:
+    print(error)
+    pass
+""
+print("first code=",timeit(code1, number=1000))
