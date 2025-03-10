a13de08986eca7e5f854d68621340f712ac99a74
nidhik2025 <nidhi.nivzen@gmail.com>
2025-03-10 18:58:38 +0530

    Add files via upload

--- classes.py
+++ classes.py
@@ -68,12 +68,68 @@ class stud:
 stn = stud(2,3)
 abn = stud(2,3)
 print(stn==abn)
+# arithmatic operation
+class std:
+    def __init__(self,a ,b):
+        self.a = a
+        self.b = b
 
-
+    def __add__(self,other):
+        return std(self.a + other.a, self.b + other.b)
+    
+s1 = std(10,20)
+s2 = std(5,6)
+sum = s1 + s2
+print((sum.a,sum.b))
+    
 # coustom containers
- 
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self,tag):
+        self.tags[tag] = self.tags.get(tag,0)+1
+
+cloud = TagCloud()
+cloud.add("Python")
+cloud.add("python")
+cloud.add("python")
+print(cloud.tags)
+# counting of iteration
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self,tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(),0)+1
+    
+    def __getitem__(self, tag):
+        self.tags.get(tag.lower(), 0)
+
+    # def __setitem__(self,key,value)
+    def __setitem__(self,tag,count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+    
+    def __iter__(self):
+        return  iter(self.tags)
 
 
+cloud = TagCloud()
+cloud["python"] = 10
+len(cloud)
+cloud.add("Python")
+cloud.add("python")
+cloud.add("python")
+print(cloud.tags)
 
-
-
+#properties
+class math:
+    def __init__(self,a,b,c,d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+    
